b5a4840ae4ce8646350adba87175afb2b9496217
coffeebreak91 <marcin@mbp-marcin.home>
2017-05-09 15:34:44 +0200

    add Product list

---
+++ js/Components/Main.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+
+class Main extends React.Component {
+    render() {
+        return <div>
+          <iframe src="https://player.vimeo.com/video/56810854" width="640" height="360" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen>
+          </iframe>
+      </div>
+    }
+}
+
+export default Main
